c16b8e1fe061579a368f1458763b0fbdcf1cec50
Mohd Inzamam <inzamamx184@gmail.com>
2026-09-19 10:02:53 +0530

    Practiceing Encapsulation

---
+++ src/BankSystem/BankAccount.java
@@ -0,0 +1,17 @@
+package BankSystem;
+public class BankAccount{
+    private int accountNumber;
+    private String accountHolderNme;
+    private double balance;
+    // Creating a constructor
+    BankAccount(int accountNumber,String accountHolderName,int balance){
+        this.accountNumber = accountNumber;
+        this.accountHolderNme = accountHolderName;
+        this.balance=balance;
+    }
+    public int deposit(double amount){
+        
+        return accountNumber;
+
+    }
+}
